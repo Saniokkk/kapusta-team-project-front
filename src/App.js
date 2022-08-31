43027c141032f13b56f648f8d1850b7334385740
TkachikNataly <tkachik.nataly@gmail.com>
2022-08-31 04:14:33 +0300

    task 15

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
 // import { ListExpenses } from "./components/ListExpenses/ListExpenses";
-// import { Layout } from "components/Layout/Layout";
+import { Layout } from "components/Layout/Layout";
 import { Routes, Route } from "react-router-dom";
-// import { BalanceReportSection } from "components/BalanceReportSection";
+import { BalanceReportSection } from "components/BalanceReportSection";
 import { ModalLogout } from "components/ModalLogout";
+import { DropDownList } from "components/DropDownList";
 
 function App() {
   return (
     <>
       <Routes>
-        {/* <Route path="/" element={<Layout />}></Route>
-        <Route path="balance" element={<BalanceReportSection />}></Route> */}
-        <Route path="/" element={<ModalLogout />}></Route>
+        <Route path="/" element={<Layout />}></Route>
+        <Route path="balance" element={<BalanceReportSection />}></Route>
+        <Route path="logout" element={<ModalLogout />}></Route>
+        <Route path="list" element={<DropDownList />}></Route>
       </Routes>
     </>
   );
