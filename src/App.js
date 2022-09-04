f7e8d051a2739f24d32118fa3deb7ed5844fd25c
TkachikNataly <tkachik.nataly@gmail.com>
2022-09-04 21:13:38 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -8,21 +8,28 @@ import { DropDownList } from "components/DropDownList";
 import { Transaction } from "components/Transaction";
 import { Summary } from "components/Summary";
 import IncomeList from "components/Income/IncomeList";
+import { Switch, Redirect } from "react-router-dom";
+import { useEffect, lazy, Suspense } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import operations from "./redux/auth/auth-operations";
+import authSelectors from "./redux/auth/auth-selector";
+import "react-toastify/dist/ReactToastify.css";
 
-function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}></Route>
-        <Route path="balance" element={<BalanceReportSection />}></Route>
-        <Route path="logout" element={<ModalLogout />}></Route>
-        <Route path="list" element={<DropDownList />}></Route>
-        <Route path="income" element={<IncomeList />}></Route>
-      </Routes>
-      <Transaction />
-      <Summary />
-    </>
-  );
-}
+import PublicRoute from
+  function App() {
+    return (
+      <>
+        <Routes>
+          <Route path="/" element={<Layout />}></Route>
+          <Route path="balance" element={<BalanceReportSection />}></Route>
+          <Route path="logout" element={<ModalLogout />}></Route>
+          <Route path="list" element={<DropDownList />}></Route>
+          <Route path="income" element={<IncomeList />}></Route>
+        </Routes>
+        <Transaction />
+        <Summary />
+      </>
+    );
+  }
 
 export default App;
