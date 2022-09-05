7b2347db4554fd0cf0722e2631dd6b0532e54be6
eijjeka <eijjeka@gmail.com>
2022-09-06 00:14:27 +0300

    add btn eye

--- src/components/ModalLogout/ModalLogout.jsx
+++ src/components/ModalLogout/ModalLogout.jsx
@@ -52,9 +52,9 @@ const ModalLogout = ({
   return createPortal(
     <div className={s.backdrop} onClick={handleOverlayClick}>
       <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{ duration: 0.6 }}
         className={s.modal}
       >
         <svg className={s.closeBtn} onClick={onClose} width={17} height={18}>
