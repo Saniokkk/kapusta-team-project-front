1f3831b583fa864d5404236c078654fd97884482
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-09-05 22:22:17 +0300

    стягиваю компоненты

--- src/components/ReportSection/ReportSection.jsx
+++ src/components/ReportSection/ReportSection.jsx
@@ -1,6 +1,70 @@
 import styles from "./ReportSection.module.css";
+import { useState, useEffect } from "react";
+import TransactionForm from "components/Transaction/TransactionForm/TransactionForm";
+import ProductList from "components/Transaction/ProductList/ProductList";
+import { nanoid } from "nanoid";
 
 const ReportSection = () => {
+  const [products, setProducts] = useState(() => {
+    //при первой загрузки получаем данные из локального хранилища либо записываем готовый массив контактов(обьектов)
+    return (
+      JSON.parse(window.localStorage.getItem("product")) ?? [
+        {
+          id: "id-1",
+          description: "Метро",
+          categories: "Транспорт",
+          sum: "8.00",
+        },
+        {
+          id: "id-2",
+          description: "Бананы",
+          categories: "Продукты",
+          sum: "30.00",
+        },
+        {
+          id: "id-3",
+          description: "Яблоки",
+          categories: "Продукты",
+          sum: "25.00",
+        },
+        {
+          id: "id-4",
+          description: "Метро",
+          categories: "Транспорт",
+          sum: "8.00",
+        },
+      ]
+    );
+  });
+
+  const addproduct = (description, categories, sum) => {
+    const product = {
+      id: nanoid(),
+      description,
+      categories,
+      sum,
+    };
+
+    setProducts((prevState) => [product, ...prevState]);
+  };
+
+  const getlist = () => {
+    return products.map((product) => {
+      return product;
+    });
+  };
+
+  const deleteContact = (Id) => {
+    setProducts((prevState) =>
+      prevState.filter((product) => product.id !== Id)
+    );
+  };
+
+  //   // первый раз при загрузки и каждый раз при изменении компонента перезаписываем в локальное хранилище
+  useEffect(() => {
+    window.localStorage.setItem("product", JSON.stringify(products));
+  }, [products]);
+
   return (
     <section className={styles.reportSection}>
       <div className={styles.reportBackgroundSection}></div>
@@ -22,45 +86,11 @@ const ReportSection = () => {
         <div className={styles.activity}>
           <div className={styles.transaction}>
             <div className={styles.transactionDate}>Date</div>
-            <div className={styles.transactionform}>Form</div>
-
-            <div className={styles.transactionformbtnIc}>
-              <div className={styles.transactionformbtn}>I</div>
-              <div className={styles.transactionformbtn}>C</div>
-            </div>
+            <TransactionForm onSubmit={addproduct} />
           </div>
 
           <div className={styles.statement}>
-            <div className={styles.transactionTable}>
-              <div className={styles.tableHeader}>
-                <div className={styles.tableHeaderline}>Дата</div>
-                <div className={styles.tableHeaderline}>Описание</div>
-                <div className={styles.tableHeaderline}>Категория</div>
-                <div className={styles.tableHeaderline}>Сумма</div>
-              </div>
-              <div className={styles.tableList}>
-                <ul>
-                  <li className={styles.tableListline}></li>
-                  <li className={styles.tableListline}></li>
-                  <li className={styles.tableListline}></li>
-                  <li className={styles.tableListline}></li>
-                </ul>
-                <ul>
-                  <li className={styles.tableListline}></li>
-                  <li className={styles.tableListline}></li>
-                  <li className={styles.tableListline}></li>
-                  <li className={styles.tableListline}></li>
-                  <li className={styles.tableListline}></li>
-                  <li className={styles.tableListline}></li>
-                  <li className={styles.tableListline}></li>
-                  <li className={styles.tableListline}></li>
-                  <li className={styles.tableListline}></li>
-                  <li className={styles.tableListline}></li>
-                  <li className={styles.tableListline}></li>
-                  <li className={styles.tableListline}></li>
-                </ul>
-              </div>
-            </div>
+            <ProductList visible={getlist()} deleteContact={deleteContact} />
             <div className={styles.summary}>Сводка</div>
           </div>
         </div>
--- src/components/Transaction/Transaction.jsx
+++ src/components/Transaction/Transaction.jsx
@@ -1,75 +1,75 @@
-import { useState, useEffect } from "react";
-import TransactionForm from "./TransactionForm/TransactionForm";
-import ProductList from "./ProductList/ProductList";
-import { nanoid } from "nanoid";
+// import { useState, useEffect } from "react";
+// import TransactionForm from "./TransactionForm/TransactionForm";
+// import ProductList from "./ProductList/ProductList";
+// import { nanoid } from "nanoid";
 
-const Transaction = () => {
-  const [products, setProducts] = useState(() => {
-    //при первой загрузки получаем данные из локального хранилища либо записываем готовый массив контактов(обьектов)
-    return (
-      JSON.parse(window.localStorage.getItem("product")) ?? [
-        {
-          id: "id-1",
-          description: "Метро",
-          categories: "Транспорт",
-          sum: "8.00",
-        },
-        {
-          id: "id-2",
-          description: "Бананы",
-          categories: "Продукты",
-          sum: "30.00",
-        },
-        {
-          id: "id-3",
-          description: "Яблоки",
-          categories: "Продукты",
-          sum: "25.00",
-        },
-        {
-          id: "id-4",
-          description: "Метро",
-          categories: "Транспорт",
-          sum: "8.00",
-        },
-      ]
-    );
-  });
+// const Transaction = () => {
+//   const [products, setProducts] = useState(() => {
+//     //при первой загрузки получаем данные из локального хранилища либо записываем готовый массив контактов(обьектов)
+//     return (
+//       JSON.parse(window.localStorage.getItem("product")) ?? [
+//         {
+//           id: "id-1",
+//           description: "Метро",
+//           categories: "Транспорт",
+//           sum: "8.00",
+//         },
+//         {
+//           id: "id-2",
+//           description: "Бананы",
+//           categories: "Продукты",
+//           sum: "30.00",
+//         },
+//         {
+//           id: "id-3",
+//           description: "Яблоки",
+//           categories: "Продукты",
+//           sum: "25.00",
+//         },
+//         {
+//           id: "id-4",
+//           description: "Метро",
+//           categories: "Транспорт",
+//           sum: "8.00",
+//         },
+//       ]
+//     );
+//   });
 
-  const addproduct = (description, categories, sum) => {
-    const product = {
-      id: nanoid(),
-      description,
-      categories,
-      sum,
-    };
+//   const addproduct = (description, categories, sum) => {
+//     const product = {
+//       id: nanoid(),
+//       description,
+//       categories,
+//       sum,
+//     };
 
-    setProducts((prevState) => [product, ...prevState]);
-  };
+//     setProducts((prevState) => [product, ...prevState]);
+//   };
 
-  const getlist = () => {
-    return products.map((product) => {
-      return product;
-    });
-  };
+//   const getlist = () => {
+//     return products.map((product) => {
+//       return product;
+//     });
+//   };
 
-  const deleteContact = (Id) => {
-    setProducts((prevState) =>
-      prevState.filter((product) => product.id !== Id)
-    );
-  };
+//   const deleteContact = (Id) => {
+//     setProducts((prevState) =>
+//       prevState.filter((product) => product.id !== Id)
+//     );
+//   };
 
-  //   // первый раз при загрузки и каждый раз при изменении компонента перезаписываем в локальное хранилище
-  useEffect(() => {
-    window.localStorage.setItem("product", JSON.stringify(products));
-  }, [products]);
+//   //   // первый раз при загрузки и каждый раз при изменении компонента перезаписываем в локальное хранилище
+//   useEffect(() => {
+//     window.localStorage.setItem("product", JSON.stringify(products));
+//   }, [products]);
 
-  return (
-    <div>
-      <TransactionForm onSubmit={addproduct} />
-      <ProductList visible={getlist()} deleteContact={deleteContact} />
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <TransactionForm onSubmit={addproduct} />
+//       <ProductList visible={getlist()} deleteContact={deleteContact} />
+//     </div>
+//   );
+// };
 
-export { Transaction };
+// export { Transaction };
