329e9741132062751a82f41b430d3c0ab1fa4451
eijjeka <eijjeka@gmail.com>
2022-09-08 01:28:52 +0300

    add google auth

--- src/components/GoogleLoader/GoogleLoader.jsx
+++ src/components/GoogleLoader/GoogleLoader.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import operations from "../../redux/auth/auth-operations";
+import { setToken } from "redux/auth/auth-slice";
 import { Container } from "../Container";
 import { useSearchParams } from "react-router-dom";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const GoogleLoader = () => {
@@ -11,14 +10,9 @@ const GoogleLoader = () => {
   const dispatch = useDispatch();
   let navigate = useNavigate();
   const token = searchParams.get("token");
-  console.log(token);
+
   useEffect(() => {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-  });
-  useEffect(() => {
-    dispatch(operations.fetchCurrentUser());
-  });
-  useEffect(() => {
+    dispatch(setToken({ token }));
     navigate("/", { replace: true });
   });
 
