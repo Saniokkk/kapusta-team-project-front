9b3e883af63a6e06a6f6ed2c6948c1bf8b04b552
Nikita Nikitenko <84923367+Nikita856294@users.noreply.github.com>
2022-09-08 09:32:10 +0300

    add save balance in redux storage

--- src/components/BalanceForm/BalanceButton.jsx
+++ src/components/BalanceForm/BalanceButton.jsx
@@ -1,7 +1,7 @@
 import styles from './BalanceForm.module.scss';
-export const BalanceBtn = ({ onSubmit }) => {
+export const BalanceBtn = ({ onSubmit}) => {
   return (
-    <button className={styles.form__button} type='submit' onSubmit={onSubmit}>
+    <button className={styles.form__button} type="submit" onSubmit={onSubmit}  >
       Підтвердити
     </button>
   );
--- src/components/BalanceForm/BalanceForm.js
+++ src/components/BalanceForm/BalanceForm.js
@@ -1,23 +1,40 @@
 import styles from "./BalanceForm.module.scss";
 import NumberFormat from "react-number-format";
-
-// import { useSelector, } from "react-redux";
-// import { getBalance } from "redux/balance/balance-operation";
+import selectors from "redux/auth/auth-selector";
+import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import authOperations from "redux/auth/auth-operations";
 
-import { useState } from "react";
+import authOperations from "redux/auth/auth-operations";
+import { ModalBalanceError } from "components/ModalBalanceError";
+import { useState, useEffect } from "react";
 import { BalanceBtn } from "./BalanceButton";
+
 import useWindowDimensions from "hooks/useWindowDimensions";
 
 const BalanceForm = () => {
   const [balance, setBalance] = useState(0);
+
   const dispatch = useDispatch();
-  // const totalBalance = useSelector(selectors.getUserBalance)
+  const totalBalance = useSelector(selectors.getUserBalance);
+
+  useEffect(() => {
+    if (totalBalance) {
+      setBalance(totalBalance);
+    }
+  }, [totalBalance]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    if (balance === 0) {
+      return <ModalBalanceError />;
+    }
+    if (totalBalance > 0) {
+      const form = e.target;
+      form.elements.input.disabled = "disable";
+      console.log(e.target.elements);
+      return;
+    }
     dispatch(authOperations.updateCurrentUser({ totalBalance: balance }));
   };
 
@@ -32,36 +49,38 @@ const BalanceForm = () => {
   const viewPort = useWindowDimensions();
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
-      <div className={styles.form_title__box}>
-        <h3 className={styles.form__title}>Баланс:</h3>
-      </div>
+    <>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.form_title__box}>
+          <h3 className={styles.form__title}>Баланс:</h3>
+        </div>
 
-      <div className={styles.form_input__box}>
-        <NumberFormat
-          className={styles.form__input}
-          autoComplete="off"
-          name="input"
-          thousandSeparator={" "}
-          suffix={" UAH"}
-          value={balance}
-          onChange={handleChange}
-          allowLeadingZeros={true}
-          isNumericString={true}
-          fixedDecimalScale={true}
-          decimalScale={2}
-        />
-        {viewPort.width > 1279 && <BalanceBtn onSubmit={handleSubmit} />}
+        <div className={styles.form_input__box}>
+          <NumberFormat
+            className={styles.form__input}
+            autoComplete="off"
+            name="input"
+            thousandSeparator={" "}
+            suffix={" UAH"}
+            value={balance}
+            onChange={handleChange}
+            allowLeadingZeros={true}
+            isNumericString={true}
+            fixedDecimalScale={true}
+            decimalScale={2}
+          />
 
-        {/* <button
+          {viewPort.width > 1279 && <BalanceBtn onSubmit={handleSubmit} />}
+          {/* <button
           className={styles.form__button}
           type='submit'
-          onSubmit={handleSubmit}
-        >
+          onSubmit={handleSubmit}>
           Підтвердити
         </button> */}
-      </div>
-    </form>
+        </div>
+      </form>
+      {balance === 0 && <ModalBalanceError />}
+    </>
   );
 };
 
