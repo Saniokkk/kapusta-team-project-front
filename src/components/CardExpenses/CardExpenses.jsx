fbc000cf04b08385578fc60a73698916c78b065e
Natali Kotelnytska <nkotelnicka@gmail.com>
2022-09-14 18:49:36 +0300

    delete console.logs

--- src/components/CardExpenses/CardExpenses.jsx
+++ src/components/CardExpenses/CardExpenses.jsx
@@ -1,25 +1,14 @@
-// import { useState } from 'react';
-// import { useSelector } from 'react-redux';
-// import { getMonth, getYear } from 'redux/extraInfo/extraInfo-selectors';
-// import { getTransactionsByCategory } from 'services/reportsApi';
-
 import spriteIcons from '../../assets/sprite-icons.svg';
 import styles from './CardExpenses.module.scss';
 
 export const CardExpenses = ({ pathIcon, title, sum, onClick }) => {
   return (
-    // <Link to={}></Link>
     <li className={styles.item}>
       <p className={styles.amount}>{sum}.00</p>
 
       <svg
         className={styles.icon}
-        // className={isActive ? 'styles.active' : styles.icon}
-        // style={({ isActive }) => (isActive ? activeClassName : null)}
-        // style={{}}
         onClick={() => {
-          // activeClassName={styles.active}
-
           onClick(pathIcon);
         }}
       >
@@ -37,34 +26,3 @@ export const CardExpenses = ({ pathIcon, title, sum, onClick }) => {
     </li>
   );
 };
-
-// const items = [
-//   {
-//     title: 'Продукти',
-//     pathIcon: 'products',
-//     sum: 5000.0,
-//     category: 'products',
-//     description: 'banana',
-//   },
-// ];
-// {
-//   "id": "630c6424c4126b2f6d97fa7f",
-//   "date": "2022-01-01T00:00:00.000Z",
-//   "owner": "_id: 630a4ce122d8de88190363c9",
-//   "description": "banana",
-//   "category": "products",
-//   "sum": 100,
-//   "createdAt": "2022-08-29T07:00:52.786Z",
-//   "updatedAt": "2022-08-29T07:00:52.786Z"
-// }
-
-// {
-//   "id": "630c6424c4126b2f6d97fa7f",
-//   "date": "2022-01-01T00:00:00.000Z",
-//   "owner": "_id: 630a4ce122d8de88190363c9",
-//   "description": "premium",
-//   "category": "Доп.дохід",
-//   "sum": 6500,
-//   "createdAt": "2022-08-29T07:00:52.786Z",
-//   "updatedAt": "2022-08-29T07:00:52.786Z"
-// }
